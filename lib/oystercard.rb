7e2be063a09ef0d3b6b52575a87a025dc1b5e95d
GarethWoodman <gareth.woodman92@gmail.com>
2020-04-20 16:54:40 +0100

    Added test for maximum balance card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,5 @@
 class Oystercard
+  LIMIT = 90
   attr_reader :balance
 
   def initialize(balance = 0)
@@ -6,7 +7,12 @@ class Oystercard
   end
 
   def top_up(amount)
-    @balance += amount
+    @balance + amount <= LIMIT ? @balance += amount : exceeds_balance
+  end
+
+  private
+  def exceeds_balance
+    raise "Exceeds balance limit of #{LIMIT}"
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,14 +1,19 @@
 require './lib/oystercard'
 
 describe Oystercard do
+  limit = Oystercard::LIMIT
+
   it "has a balance" do
     expect(subject).to respond_to(:balance)
   end
 
   it "tops up the card with a specified balance" do
-    #card = Oystercard.new
     subject.top_up(10)
     expect(subject.balance).to eq 10
+  end
 
+  it "returns exceed limit error if top_up exceeds balance limit of #{limit}" do
+    subject.top_up(50)
+    expect { subject.top_up(50) }.to raise_error("Exceeds balance limit of #{limit}")
   end
 end
